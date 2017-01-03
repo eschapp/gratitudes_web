8d807f0a4c01d62e4ce8550b02a6d8f0f7c246fe
eschapp <eschapp@gmail.com>
2017-01-03 12:49:01 -0600

    moved firebase init to correct entry point

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import * as firebase from 'firebase';
-
-// Make sure you swap this out with your Firebase app's config
-const config = {
-  apiKey: "AIzaSyCRJZOv6J8MYtU_-CraYyC72Ttv7loyzEQ",
-  authDomain: "gratitudes-ff569.firebaseapp.com",
-  databaseURL: "https://gratitudes-ff569.firebaseio.com",
-  storageBucket: "gratitudes-ff569.appspot.com",
-};
-
-
-const gratitudes = firebase.initializeApp(config).database().ref();
-
 
 
 class App extends Component {
