6e51240735ab0f8cec51f3e1732691354edcbc13
eschapp <eschapp@gmail.com>
2017-01-05 10:25:37 -0600

    working towards the correct firebase connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase';
 
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCRJZOv6J8MYtU_-CraYyC72Ttv7loyzEQ",
+  authDomain: "gratitudes-ff569.firebaseapp.com",
+  databaseURL: "https://gratitudes-ff569.firebaseio.com",
+  storageBucket: "gratitudes-ff569.appspot.com",
+};
+const gratitudes_firebase = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: {title: 'Bread'}
+    };
+    // this.gratRef = this.getRef().child('items');
+
+  }
+  // getRef() {
+  //   return gratitudes_firebase.database().ref();
+  // }
+  //
+  // listenForItems(itemsRef) {
+  //     const rootRef = gratitudes_firebase.database().ref().child('gratitudes-ff569');
+  //     const gratRef = rootRef.child('items');
+  //     // itemsRef.on('value', (snap) => {
+  //     gratRef.on('value', (snap) => {
+  //
+  //       // get children as an array
+  //       var items = [];
+  //       snap.forEach((child) => {
+  //         items.push({
+  //           title: child.val().title,
+  //           _key: child.key
+  //         });
+  //       });
+  //       this.setState({
+  //         items: this.state.items
+  //       });
+  //       // this.setState({
+  //       //   dataSource: this.state.dataSource.cloneWithRows(items)
+  //       // });
+  //
+  //     });
+  //   }
+  //
+  //   componentDidMount() {
+  //     // this.listenForItems(this.itemsRef);
+  //     this.listenForItems(this.gratRef);
+  //
+  //   }
+componentDidMount() {
+  const rootRef = gratitudes_firebase.database().ref().child('gratitudes-ff569');
+  const gratRef = rootRef.child('items');
+  gratRef.on('value', snap => {
+    this.setState({
+      items: {title: snap.val()}
+    });
+  });
+}
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to Gratitudes</h2>
+        </div>
+        <div className="App-List">
+          <h3>Recent Gratitudes</h3>
+          <h1>{this.state.items.title}</h1>
+
         </div>
 
       </div>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import * as firebase from 'firebase';
 
-const config = {
-  apiKey: "AIzaSyCRJZOv6J8MYtU_-CraYyC72Ttv7loyzEQ",
-  authDomain: "gratitudes-ff569.firebaseapp.com",
-  databaseURL: "https://gratitudes-ff569.firebaseio.com",
-  storageBucket: "gratitudes-ff569.appspot.com",
-};
-
-firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
